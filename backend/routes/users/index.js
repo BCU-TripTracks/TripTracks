f431b87f867619c60e850b41ca670c48aac52f10
SLTechYouungs <leeyoungjae2001@icloud.com>
2024-03-11 20:06:53 +0900

    TagSetup Api 개발 완료

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -1,11 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.post('/', ()=>{
-
-});
-router.use('/Login', require("./Login.js"));
-router.use('/SignUp', require("./SignUp.js"));
-router.use('/LifeThemeSetup', require("./LifeThemeSetup.js"));
+router.post("/", () => {});
+router.use("/Login", require("./Login.js"));
+router.use("/SignUp", require("./SignUp.js"));
+router.use("/TagSetup", require("./TagSetup.js"));
 
 module.exports = router;
