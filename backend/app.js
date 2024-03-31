5fd2649e5bc7b10f8b3777ba86c9c906a293e6f8
SLTechYouungs <leeyoungjae2001@icloud.com>
2024-04-01 00:36:58 +0900

    세션설정수정

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
+const session = require("express-session");
 
 var apiRouter = require("./routes/index");
 
@@ -25,6 +26,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "triptracks_key", // 세션을 안전하게 유지하는 데 사용되는 비밀키
+    resave: false, // 세션을 항상 저장할지 여부 (변경되지 않았어도)
+    saveUninitialized: true, // 초기화되지 않은 세션을 저장소에 저장할지 여부
+    cookie: {
+      secure: false, // true로 설정하면 HTTPS를 통해서만 쿠키 전송
+      httpOnly: true, // 클라이언트 측 JavaScript가 문서의 쿠키를 읽을 수 없음
+      maxAge: 1000 * 60 * 60, // 쿠키 유효 시간 (여기서는 1시간)
+    },
+  })
+);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/apidoc", express.static(path.join(__dirname, "apidoc")));
