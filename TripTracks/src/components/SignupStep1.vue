6ea43b9d74df364a526b2d86f5cf65ba0a99e6d1
eggegg96 <uyoenu0326@gmail.com>
2024-04-02 03:12:17 +0900

    복구

---
+++ TripTracks/src/components/SignupStep1.vue
@@ -0,0 +1,134 @@
+<script setup>
+import { watch, ref } from "vue";
+import { useRouter } from "vue-router";
+
+import axios from "../axios";
+
+const router = useRouter();
+
+const input_Email = ref("");
+const input_Pwd = ref("");
+const input_Pwd_Ch = ref("");
+const input_Name = ref("");
+
+const email_check = ref({ EmailFormat: true, EmailAlread: true });
+
+watch(input_Email, (newValue) => {
+  // 여기서 newValue는 업데이트된 inputEmail 값이고, oldValue는 이전 값입니다.
+
+  // 이메일 유효성 검사 로직을 여기에 추가할 수 있습니다.
+  // 예: 간단한 정규 표현식을 사용한 이메일 형식 검증
+  if (input_Email.value == "")
+    return (email_check.value = { EmailAlread: true, EmailFormat: true });
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailPattern.test(newValue)) {
+    email_check.value.EmailFormat = true;
+    axios
+      .post("/users/check_email", { user_Email: input_Email.value })
+      .then((req) => {
+        const { success } = req.data;
+        if (success == true) {
+          email_check.value.EmailAlread = true;
+        }
+      })
+      .catch((err) => {
+        const result = err.response.data;
+        if (result.err_Code == "EmailAlreadyExists")
+          email_check.value.EmailAlread = false;
+      });
+  } else {
+    email_check.value.EmailFormat = false;
+  }
+});
+
+const singup_Button_Click = () => {
+  if (input_Pwd.value !== input_Pwd_Ch.value)
+    return console.log("비밀번호 불일치");
+  if (!email_check.value.EmailAlread || !email_check.value.EmailFormat)
+    return console("이메일 인증 문제");
+  axios
+    .post("/users/signup", {
+      user_Email: input_Email.value,
+      user_Passwd: input_Pwd.value,
+      user_Name: input_Name.value,
+    })
+    .then((req) => {
+      const result = req.data;
+      console.log(result);
+      if (result.message === "success") {
+        localStorage.setItem("user_Email", input_Email.value);
+        return router.push({ name: "Step2" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+</script>
+
+<template>
+  <div class="Infobox">
+    <input
+      class="Email"
+      type="email"
+      placeholder="Email"
+      v-model="input_Email"
+    />
+    <p v-if="!email_check.EmailFormat">이메일 형식이 아닙니다.</p>
+    <p v-if="!email_check.EmailAlread">중복된 이메일 입니다.</p>
+    <input
+      class="Passwd mt20"
+      type="pwd"
+      placeholder="Password"
+      v-model="input_Pwd"
+    />
+    <input
+      class="PasswdCh mb20"
+      type="pwdcheck"
+      placeholder="Password Check"
+      v-model="input_Pwd_Ch"
+    />
+    <p v-if="input_Pwd != input_Pwd_Ch">비밀번호가 일치하지 않습니다.</p>
+    <input
+      class="Name mb20"
+      type="name"
+      placeholder="Name"
+      v-model="input_Name"
+    />
+    <button @click="singup_Button_Click()">Next Level</button>
+  </div>
+</template>
+
+<style scoped>
+.Infobox {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+}
+.Infobox > p {
+  color: red;
+}
+.Infobox > input {
+  height: 4rem;
+  border-radius: 7px;
+  padding: 0 1rem;
+  border: 1px var(--login-input-border-color) solid;
+  background-color: var(--login-input-background-color);
+  color: white;
+}
+.Infobox > .Passwd {
+  border-bottom: 1px var(--login-input-border-color) solid;
+  border-radius: 7px 7px 0 0;
+}
+.Infobox > .PasswdCh {
+  border-radius: 0 0 7px 7px;
+}
+.Infobox > button {
+  height: 4rem;
+  border-radius: 7px;
+  background-color: var(--login-button-background-color);
+  border: none;
+  color: var(--login-text-color);
+  font-size: 23px;
+}
+</style>
---
+++ TripTracks/src/components/write copy 2.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from "vue";
+
+const tag = ref("");
+const results = ref([]);
+
+const printAndClear = () => {
+  results.value.push(tag.value);
+  tag.value = "";
+};
+</script>
+
+<template>
+  <div class="writecontainer">
+    <div class="tagbox">
+      <input
+        type="text"
+        v-model="tag"
+        @keyup.enter="printAndClear"
+        placeholder="Tag"
+      />
+      <span id="result">{{ results.join(", ") }}</span>
+    </div>
+    <div class="articlecontainer">
+      <div class="articlebox">
+        <div class="box">1</div>
+        <div class="box">1</div>
+        <div class="box">1</div>
+        <div class="box">1</div>
+      </div>
+    </div>
+
+    <div class="articlecomment">
+      <input type="text" placeholder="comment를 입력하세요." />
+    </div>
+  </div>
+</template>
+<style scoped>
+.writecontainer {
+  display: flex;
+  height: 600px;
+  width: 800px;
+  flex-direction: column;
+  margin: auto;
+}
+.tagbox {
+  margin-bottom: 10px;
+}
+.articlecontainer {
+  display: flex;
+}
+.articlebox {
+  display: grid;
+  height: 600px;
+  width: 800px;
+  border: 1px black solid;
+  grid-template-columns: 3fr 1fr; /* 변경된 부분 */
+  grid-template-rows: 1fr;
+}
+.articlebox > .box:first-child {
+  grid-column: 600px;
+  grid-row: 600px;
+  border: 1px black;
+  background-color: red;
+}
+.big-box {
+  grid-column: span 2;
+  border: 1px black;
+  background-color: red;
+}
+.small-box {
+  border: 1px black;
+}
+.articlecomment {
+  margin-top: 10px;
+}
+</style>
