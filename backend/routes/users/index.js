83efeacb1cb3920b66989179dce52b99160b076f
shj7157 <ana3630@naver.com>
2024-04-08 19:36:15 +0900

    socketIO

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -9,6 +9,5 @@ router.use("/TagSetup", require("./TagSetup.js"));
 router.use("/Check_Email", require("./Check_Email.js"));
 router.use("/Top_Tags", require("./Top_Tags.js"));
 router.use("/Top_Post", require("./Top_Post.js"));
-router.use("/Direct_Message", require("./Direct_Message.js"));
 router.use("/Logout", require("./Logout.js"));
 module.exports = router;
