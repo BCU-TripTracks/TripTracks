9c78a452378a142ef9a02216496fa0d524a954dd
SLTechYouungs <leeyoungjae2001@icloud.com>
2024-05-02 23:21:55 +0900

    메시지 수신 이후 하단 고정

--- backend/routes/direct/index.js
+++ backend/routes/direct/index.js
@@ -5,6 +5,7 @@ router.post("/", () => {});
 router.use("/send_Message", require("./send_message.js"));
 router.use("/print_Room", require("./print_Room.js"));
 router.use("/print_DM", require("./print_DM.js"));
+router.use("/print_DM_Next", require("./print_DM_Next.js"));
 router.use("/make_room", require("./make_room.js"));
 
 module.exports = router;
