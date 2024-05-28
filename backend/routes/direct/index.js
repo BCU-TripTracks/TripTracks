5fb0cdd0c0c950cf6320ff6b2564218060656ac8
SLTechYouungs <leeyoungjae2001@icloud.com>
2024-05-28 16:53:37 +0900

    22

--- backend/routes/direct/index.js
+++ backend/routes/direct/index.js
@@ -6,7 +6,6 @@ router.use("/send_Message", require("./send_message.js"));
 router.use("/print_Room", require("./print_Room.js"));
 router.use("/print_DM", require("./print_DM.js"));
 router.use("/print_DM_Next", require("./print_DM_Next.js"));
-router.use("/make_room", require("./make_room.js"));
 router.use("/search_Room", require("./search_Room.js"));
 
 module.exports = router;
--- backend/routes/feeds/index.js
+++ backend/routes/feeds/index.js
@@ -2,8 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 router.post("/", () => {});
-router.use("/Img_save", require("./Img_save.js"));
-router.use("/Img_send", require("./Img_send.js"));
 router.use("/Post_save", require("./Post_save.js"));
 router.use("/Posts_list", require("./Posts_list.js"));
 router.use("/Post_detail", require("./Post_detail.js"));
