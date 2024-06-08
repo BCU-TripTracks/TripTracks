c9a0947e48da157bca961b5883af959ab2c953d9
SLTechYouungs <leeyoungjae2001@icloud.com>
2024-06-08 18:48:54 +0900

    개인 작업 포트 설정

--- TripTracks/src/socket.js
+++ TripTracks/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://triptracks.co.kr");
-// const socket = io("http://localhost:6510");
+// const socket = io("http://triptracks.co.kr");
+const socket = io("http://localhost:6510");
 socket.on("connect", () => {
   console.log("Connected to the server");
 });
