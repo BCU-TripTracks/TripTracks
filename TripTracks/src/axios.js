87fa7230b6e9d9932d17b790ec16477c812ff1c5
SLTechYouungs <leeyoungjae2001@icloud.com>
2024-06-08 21:36:49 +0900

    포트 되돌리기 완료

--- TripTracks/src/axios.js
+++ TripTracks/src/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // 여기에 설정을 추가합니다.
-  // baseURL: "http://triptracks.co.kr/api",
-  baseURL: "http://localhost:6510/api",
+  baseURL: "http://triptracks.co.kr/api",
+  // baseURL: "http://localhost:6510/api",
 });
 
 export default axiosInstance;
